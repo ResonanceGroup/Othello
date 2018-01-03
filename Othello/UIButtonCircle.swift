d866bba5622cb33d1212f059c2005898a13f2a8d
MacUser <macuser@macusers-imac.local>
2018-01-03 11:32:14 -0800

    Made circle out of button

---
+++ Othello/UIButtonCircle.swift
@@ -0,0 +1,14 @@
+//
+//  UIButtonCircle.swift
+//  Othello
+//
+//  Created by MacUser on 1/3/18.
+//  Copyright © 2018 MacUser. All rights reserved.
+//
+
+import Foundation
+class UIButtonRounded: ViewController
+{
+    
+    
+}
